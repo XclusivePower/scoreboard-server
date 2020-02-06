16c4bdd6c5d4584e4477db1175b2b6486ba9b846
ToXclusive <lucas.perssonmattsson@jgy.se>
2020-02-06 13:34:17 +0100

    Added libs, set up server

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 
 
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+
 const app = express();
 const PORT = (process.env.PORT || 5000);
 
+app.use(helmet());
+app.use(bodyParser.json({type : 'application/*+json'}));
 
+app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-with, Content-type, Accept");
+    next();
+});
+
+const homeRoute = require("./routes/home");
+app.use(homeRoute);
+
+const userRoute = require("./routes/user");
+app.use(userRoute);
 
 app.listen(PORT, () =>{
     console.log("server started on port : " + PORT);
